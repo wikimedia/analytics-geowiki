9c4c42328170525ffd9de9727a869fe4e00706c4
Antoine Musso <hashar@free.fr>
2014-05-20 14:13:48 +0200

    Lint setup.py
    * drops unused import setuptools.find_packages()
    * fix up whitespaces
    
    Change-Id: I5e1b6eddbefaed13c699dbb9b1105e5b2c6e3379

--- setup.py
+++ setup.py
@@ -1,36 +1,36 @@
 #!python
 # -*- coding: utf-8 -*-
-from setuptools import setup, find_packages
+from setuptools import setup
 
-readme = open('readme.md','rU').read()
+readme = open('readme.md', 'rU').read()
 
 setup(
-    name                    = 'geowiki',
-    version                 = '0.1.0',
-    description             = 'Geo Coding module',
-    long_description        = readme,
-    url                     = 'https://gerrit.wikimedia.org/r/gitweb?p=analytics/editor-geocoding.git;a=summary',
+    name='geowiki',
+    version='0.1.0',
+    description='Geo Coding module',
+    long_description=readme,
+    url='https://gerrit.wikimedia.org/r/gitweb?p=analytics/editor-geocoding.git;a=summary',
 
-    author                  = 'Declerambaul',
-    author_email            = '{otto,dvanliere,fkaelin,dsc,erosen}@wikimedia.org',
+    author='Declerambaul',
+    author_email='{otto,dvanliere,fkaelin,dsc,erosen}@wikimedia.org',
 
-    packages                = ['geowiki',],
-    # package_dir           = {'geowiki': 'geowiki'},
+    packages=['geowiki'],
+    # package_dir={'geowiki': 'geowiki'},
 
-    package_data            = {'geowiki': ['data/*.bots', 'data/*.csv']},
-    include_package_data    = True,
-    install_requires        = [
-                                "argparse >=1.2.1",
-                                "MySQL-python >= 1.2.3",
-                                "geoip",
-                                "gcat == 0.1.0",
-                                ],
-    entry_points            = {
-                                'console_scripts': ['geowiki = geowiki.process_data:main']
-                                },
-    dependency_links        = [
-                                "http://github.com/wikimedia/limnpy/tarball/master#egg=limnpy-0.1.0",
-                                "http://github.com/embr/wikipandas/tarball/master#egg=wikipandas-0.0.1"
-                                ],
-    classifiers              = [],
+    package_data={'geowiki': ['data/*.bots', 'data/*.csv']},
+    include_package_data=True,
+    install_requires=[
+        "argparse >=1.2.1",
+        "MySQL-python >= 1.2.3",
+        "geoip",
+        "gcat == 0.1.0",
+    ],
+    entry_points={
+        'console_scripts': ['geowiki=geowiki.process_data:main']
+    },
+    dependency_links=[
+        "http://github.com/wikimedia/limnpy/tarball/master#egg=limnpy-0.1.0",
+        "http://github.com/embr/wikipandas/tarball/master#egg=wikipandas-0.0.1"
+    ],
+    classifiers=[],
 )
